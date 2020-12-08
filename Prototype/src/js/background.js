97f0abd69d8ad0e0a56171cc334e5f5fe51a5e2c
roy <roy.giladi@oolo.io>
2020-12-08 15:35:13 +0200

    add background script

---
+++ Prototype/src/js/background.js
@@ -0,0 +1,48 @@
+/**
+ * background.js
+ * 
+ * this file will handle events messaging between all the different parts of the extension
+ */
+
+const EVENT_OPEN_MAIN_TAB = 'PROFESSOR_PREBID_OPEN_MAIN'
+const EVENT_AUCTION_DATA = 'PROFESSOR_PREBID_AUCTION_DATA'
+const EVENT_MAIN_PAGE_REQUEST_DATA = 'PROFESSOR_PREBID_MAIN_PAGE_REQUEST_DATA'
+
+let mainTabId
+let tempData
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  const msgType = message && message.type
+
+  switch (msgType) {
+    case EVENT_OPEN_MAIN_TAB:
+      if (mainTabId) {
+        chrome.tabs.update(mainTabId, { active: true  }, function (tab) {
+          mainTabId = tab.id
+        })
+      } else {
+        chrome.tabs.create({ url: './main.html' }, function (tab) {
+          mainTabId = tab.id
+          console.log('created tab with tabId: ', mainTabId)
+        })
+      }
+      break
+    case EVENT_AUCTION_DATA:
+      console.log(`received event: ${EVENT_AUCTION_DATA}`, message)
+      console.log('storing payload in a temp variable')
+      tempData = message.payload
+      break
+    case EVENT_MAIN_PAGE_REQUEST_DATA:
+      sendResponse(tempData)
+      break
+  }
+})
+
+// handle main tab closing
+chrome.tabs.onRemoved.addListener(function (tabId) {
+  if (tabId === mainTabId) {
+    console.log('main tab is closed, clearing mainTabId & tempData')
+    mainTabId = undefined
+    tempData = undefined
+  }
+})
