3251ba88fb03022b514e35ff07233ab701f98460
Chris Huie <phoenixtechnerd@gmail.com>
2021-08-31 06:05:25 -0700

    add placeholder for main page routing

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -89,6 +89,24 @@ export const Popup = () => {
           <button onClick={handleOpenMainTab}>open main tab</button>
         </div>
       </main>
+      {/* <div className="component-links">
+        {<div className="component-links">
+          <div>
+            <p>Stats</p>
+            <i className="fas fa-camera"></i>
+            <i></i>
+          </div>
+          <div>
+            <p>Timeline</p>
+            <i className="fas fa-camera"></i>
+            <i></i>
+          </div>
+          <div>
+            <p>Config</p>
+            <i className="fas fa-camera"></i>
+            <i></i>
+          </div>
+        </div> */}
     </div>
   );
 };
