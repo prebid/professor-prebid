e488ef98de6c75e92fcf753011b2053040590085
Florian Erl <46747754+florianerl@users.noreply.github.com>
2021-09-16 13:29:32 +0200

    disable logs

--- src/logger.ts
+++ src/logger.ts
@@ -4,7 +4,7 @@ const LOG_PREFIX = '%c[PROFESSOR_PREBID]';
 const LOG_COLOR = 'color: white; background-color: #ff6f00;';
 
 class Logger {
-  enabled = !false;
+  enabled = false;
 
   log(...args: any[]) {
     if (this.enabled) {
