56890c307c56f091d474e4ef2335a10046da4bba
Florian Erl <46747754+florianerl@users.noreply.github.com>
2021-09-16 13:21:20 +0200

    add eventlisteners, store data in variable

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,17 +1,24 @@
 import constants from '../constants.json';
 import logger from '../logger';
+import { IGoogleAdManagerDetails } from '../inject/scripts/googleAdManager';
+import { IPrebidDetails } from '../inject/scripts/prebid';
+import { ITcfDetails } from '../inject/scripts/tcf';
 
 let mainTabId: number;
+let appData: any;
+let gamDetails: IGoogleAdManagerDetails;
+let prebidDetails: IPrebidDetails;
+let tcfDetails: ITcfDetails;
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const msgType = message && message.type;
   const payload = message && message.payload;
-
   switch (msgType) {
     case constants.EVENTS.OPEN_DATA_TAB:
       if (mainTabId) {
         chrome.tabs.update(mainTabId, { active: true, url: `./app.html` }, (tab) => {
-          mainTabId = tab.id;
+          mainTabId = tab?.id;
+          logger.log('[Background] update tab with tabId: ', mainTabId);
           sendResponse();
         });
       } else {
@@ -23,16 +30,42 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       }
 
       return true;
+      break;
     case constants.EVENTS.SEND_DATA_TO_BACKGROUND:
-      chrome.storage.local.set({ [constants.LOCAL_DATA]: message.payload });
+      logger.log('[Background] received data: ', payload)
+      appData = { ...payload };
       sendResponse();
       break;
     case constants.EVENTS.REQUEST_DATA_FROM_BACKGROUND:
-      chrome.storage.local.get(constants.LOCAL_DATA, (result) => {
-        const data = result ? result[constants.LOCAL_DATA] : null;
-        sendResponse(data);
-      });
-
+      logger.log('[Background] send data: ', appData)
+      sendResponse(appData);
+      break;
+    case constants.EVENTS.SEND_GAM_DETAILS_TO_BACKGROUND:
+      logger.log('[Background] received gam details data:', payload)
+      gamDetails = { ...payload };
+      sendResponse();
+      break;
+    case constants.EVENTS.REQUEST_GAM_DETAILS_FROM_BACKGROUND:
+      logger.log('[Background] send gam details data:', payload)
+      sendResponse(gamDetails);
+      break;
+    case constants.EVENTS.SEND_PREBID_DETAILS_TO_BACKGROUND:
+      logger.log('[Background] received prebid details data:', payload)
+      prebidDetails = { ...payload };
+      sendResponse();
+      break;
+    case constants.EVENTS.REQUEST_PREBID_DETAILS_FROM_BACKGROUND:
+      logger.log('[Background] send prebid details data:', payload)
+      sendResponse(prebidDetails);
+      break;
+    case constants.EVENTS.SEND_TCF_DETAILS_TO_BACKGROUND:
+      logger.log('[Background] received tcf details data:', payload)
+      tcfDetails = { ...payload };
+      sendResponse();
+      break;
+    case constants.EVENTS.REQUEST_TCF_DETAILS_FROM_BACKGROUND:
+      logger.log('[Background] send tcf details data:', payload)
+      sendResponse(tcfDetails);
       break;
     default:
       sendResponse();
