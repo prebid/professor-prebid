41edccf98ed7aa9128f2a7d98f15250c06d441d4
Florian Erl <46747754+florianerl@users.noreply.github.com>
2021-10-04 14:05:37 +0200

    use prebid events

--- src/pages/App/components/InfoComponent.tsx
+++ src/pages/App/components/InfoComponent.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { IGoogleAdManagerDetails } from "../../../inject/scripts/googleAdManager";
 import { IPrebidDetails } from "../../../inject/scripts/prebid";
 
-
 class InfoComponent extends React.Component<IInfoComponentProps> {
   render() {
     const { prebid, googleAdManager } = this.props;
+    const allAvailableBids = prebid.events.filter(event => event.eventType === 'bidResponse');
+    const allNoBids = prebid.events.filter(event => event.eventType === 'noBid');
+    const allAdUnits = Array.from(new Set(prebid.events.filter(event => event.eventType === 'auctionInit').reduce((previousValue, currentValue) => [...previousValue, ...currentValue.args.adUnitCodes], [])))
+    const allBidders = Array.from(new Set([...allAvailableBids, ...allNoBids].map(event => event.args.bidder)))
+
     return (
       <div>
         <ul className="stats-list">
-          <li>Ads Detected: {googleAdManager.slots.length}</li>
-          <li>Bidders: {Array.from(new Set(prebid.bids.map(bid => bid.bidder))).length}</li>
-          <li>
-            No Bid Ratio: {prebid.bids.filter(bid => bid.statusMessage === 'no bid').length} / {prebid.bids.filter(bid => bid.statusMessage === 'Bid available').length}
-          </li>
+          <li>GAM Slots Detected: {googleAdManager.slots.length}</li>
+          <li>AdUnits Detected: {allAdUnits.length}</li>
+          <li>Bidders: {allBidders.length}</li>
+          <li>NoBid / Bid Ratio: {allNoBids.length} / {allAvailableBids.length}</li>
         </ul>
       </div>
     );
