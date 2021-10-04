579541bab5c560b79e6638cba9b55a4b16b75fa0
Florian Erl <46747754+florianerl@users.noreply.github.com>
2021-10-04 14:04:26 +0200

    use prebid events

--- src/pages/App/components/TimelineComponent.tsx
+++ src/pages/App/components/TimelineComponent.tsx
@@ -3,97 +3,81 @@ import { IGoogleAdManagerDetails } from "../../../inject/scripts/googleAdManager
 import React from 'react';
 
 class TimeLine extends React.Component<ITimelineComponentProps> {
-  timelineStartTimestamp: number = null;
-  timelineEndTimestamp: number = null;
-  timelineLength: number = null;
-  timelinePadding: number = 100;
+  timelinePadding = 50;
 
-  updateTimelineTimestamps = (input: number[]) => {
-    input.forEach(timestamp => {
-      if (timestamp < this.timelineStartTimestamp || this.timelineStartTimestamp === null) {
-        this.timelineStartTimestamp = timestamp;
+  updateTimelineTimestamps = (timestampArray: number[]) => {
+    let timelineStartTimestamp: number = null;
+    let timelineEndTimestamp: number = null;
+    timestampArray.forEach(timestamp => {
+      if (timestamp < timelineStartTimestamp || timelineStartTimestamp === null) {
+        timelineStartTimestamp = timestamp;
       }
-      if (timestamp > this.timelineEndTimestamp || this.timelineEndTimestamp === null) {
-        this.timelineEndTimestamp = timestamp;
+      if (timestamp > timelineEndTimestamp || timelineEndTimestamp === null) {
+        timelineEndTimestamp = timestamp;
       }
-      this.timelineLength = (this.timelineEndTimestamp - this.timelineStartTimestamp);
     })
+    return { timelineStartTimestamp, timelineEndTimestamp }
   }
 
-  calculateMarginLeft = (requestTimestamp: number): string => {
-    return `${(requestTimestamp - this.timelineStartTimestamp + this.timelinePadding) / (this.timelineLength + 2 * this.timelinePadding) * 100}%`
+  calculateMarginLeft = (requestTimestamp: number, timelineStartTimestamp: number, timelineLength: number): string => {
+    return `${(requestTimestamp - timelineStartTimestamp + this.timelinePadding) / (timelineLength + 2 * this.timelinePadding) * 100}%`
   }
 
-  calculateMarginRight = (responseTimestamp: number): string => {
-    return `${(this.timelineEndTimestamp - responseTimestamp + this.timelinePadding) / (this.timelineLength + 2 * this.timelinePadding) * 100}%`
+  calculateMarginRight = (responseTimestamp: number, timelineEndTimestamp: number, timelineLength: number): string => {
+    return `${(timelineEndTimestamp - responseTimestamp + this.timelinePadding) / (timelineLength + 2 * this.timelinePadding) * 100}%`
   }
 
   generateBidderRows = (prebid: IPrebidDetails) => {
-    return Object.keys(prebid.events.bidders).map(key => {
-      const bidder = prebid.events.bidders[key];
-      return {
-        label: key,
-        offset: bidder.responseTimestamp - bidder.requestTimestamp,
-        requestTimestamp: bidder.requestTimestamp,
-        responseTimestamp: bidder.responseTimestamp,
-        marginLeft: this.calculateMarginLeft(bidder.requestTimestamp),
-        marginRight: this.calculateMarginRight(bidder.responseTimestamp)
-      }
-    }).sort((a, b) => a.requestTimestamp - b.requestTimestamp)
-  }
+    const allBidderEvents: any[] = [];
+    const allEventTimestamps: number[] = [];
+    prebid.events
+      .filter(event => ['bidRequested', 'bidResponse', 'noBid'].includes(event.eventType))
+      .forEach(event => {
+        const index: number = allBidderEvents.findIndex(bidderRequest =>
+          event.args.auctionId === bidderRequest.args.auctionId
+          && (event.args.bidderCode === bidderRequest.args.bidderCode || event.args.bidder === bidderRequest.args.bidderCode));
+        switch (event.eventType) {
+          case 'bidRequested': {
+            allEventTimestamps.push(event.args.start);
+            allBidderEvents.push({ ...event, elapsedTime: event.elapsedTime });
+            break;
+          }
+          case 'bidResponse': {
+            allEventTimestamps.push(event.args.requestTimestamp);
+            allEventTimestamps.push(event.args.responseTimestamp);
+            allBidderEvents[index].args.endTimestamp = event.args.responseTimestamp;
+            break;
+          }
+          case 'noBid': {
+            allBidderEvents[index].args.endTimestamp = Math.floor(allBidderEvents[index].args.start + event.elapsedTime - allBidderEvents[index].elapsedTime);
+            break
+          }
+        }
+      });
 
-  generateGoogleAdManagerSlotRows = (googleAdManager: IGoogleAdManagerDetails) => {
-    return Object.keys(googleAdManager.slotEvents).map(key => {
-      const slotEventsArray = googleAdManager.slotEvents[key];
-      // use postAuctionStartTimestamp and postAuctionEndTimestamp as fallback if events are missing
-      const slotRequestedTimestamp = slotEventsArray.find(event => event.type === 'slotRequested')?.timestamp;
-      const slotResponseReceivedTimestamp = slotEventsArray.find(event => event.type === 'slotResponseReceived')?.timestamp;
-      return {
-        label: key,
-        offset: slotResponseReceivedTimestamp - slotRequestedTimestamp,
-        requestTimestamp: slotRequestedTimestamp,
-        responseTimestamp: slotResponseReceivedTimestamp,
-        marginLeft: this.calculateMarginLeft(slotRequestedTimestamp),
-        marginRight: this.calculateMarginRight(slotResponseReceivedTimestamp)
-      }
-    }).sort((a, b) => a.requestTimestamp - b.requestTimestamp);
-  }
+    const { timelineStartTimestamp, timelineEndTimestamp } = this.updateTimelineTimestamps(allEventTimestamps);
+    const timelineLength = timelineEndTimestamp - timelineStartTimestamp;
 
-  generatePostAuctionRows = (googleAdManager: IGoogleAdManagerDetails) => {
-    return [
-      {
-        label: 'google-ad-manager',
-        offset: googleAdManager.postAuctionEndTimestamp - googleAdManager.postAuctionStartTimestamp,
-        requestTimestamp: googleAdManager.postAuctionStartTimestamp,
-        responseTimestamp: googleAdManager.postAuctionEndTimestamp,
-        marginLeft: this.calculateMarginLeft(googleAdManager.postAuctionStartTimestamp),
-        marginRight: this.calculateMarginRight(googleAdManager.postAuctionEndTimestamp)
-      }
-    ]
+    return allBidderEvents
+      .map(bidderRequest => ({
+        label: bidderRequest.args.bidderCode,
+        offset: bidderRequest.args.endTimestamp - bidderRequest.args.start,
+        requestTimestamp: bidderRequest.args.start,
+        responseTimestamp: bidderRequest.args.endTimestamp,
+        marginLeft: this.calculateMarginLeft(bidderRequest.args.start, timelineStartTimestamp, timelineLength),
+        marginRight: this.calculateMarginRight(bidderRequest.args.endTimestamp, timelineEndTimestamp, timelineLength)
+      }))
+      .sort((a, b) => a.requestTimestamp - b.requestTimestamp)
   }
 
   render = () => {
-    let { prebid, googleAdManager } = this.props;
-    // get lowest and highest timestamp for timeline start / stop 
-    this.updateTimelineTimestamps([
-      prebid.events.auctionStartTimestamp,
-      prebid.events.auctionEndTimestamp,
-      googleAdManager.postAuctionStartTimestamp,
-      googleAdManager.postAuctionEndTimestamp
-    ]);
-
-    const dataRows = [
-      ...this.generateBidderRows(prebid),
-      ...this.generatePostAuctionRows(googleAdManager),
-      // ...this.generateGoogleAdManagerSlotRows(googleAdManager), // un-comment to add lines/slot TODO handle missing events properly
-    ];
-
+    let { prebid } = this.props;
     return (
       <table style={{ width: '100%', border: '1px dotted black' }}>
         <tbody>
           <tr style={{}}>
             <td >
-              {dataRows.map((row, index) =>
+              {this.generateBidderRows(prebid).map((row, index) =>
                 <div
                   style={{
                     border: '1px solid orange',
@@ -101,7 +85,9 @@ class TimeLine extends React.Component<ITimelineComponentProps> {
                     marginRight: row.marginRight,
                     overflow: 'visible',
                     maxHeight: '20px',
-                    fontSize: '10px'
+                    fontSize: '10px',
+                    marginTop: '5px',
+                    marginBottom: '5px',
                   }}
                   key={index}
                 >
