3a97e79f19c00f6161bbbc5572f6b0fdbc164e9f
Florian Erl <46747754+florianerl@users.noreply.github.com>
2021-10-19 11:56:13 +0200

    check undefined, parse timestamps

--- src/pages/App/components/details/PrebidDetailsComponent.tsx
+++ src/pages/App/components/details/PrebidDetailsComponent.tsx
@@ -6,28 +6,28 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 const PrebidDetailsComponent = ({ prebid }: IPrebidDetailsComponentProps): JSX.Element => {
-  const auctionEndEvents = prebid?.events.filter(event => event.eventType === 'auctionEnd') || [];
-  const allAvailableBids = prebid?.events?.filter(event => event.eventType === 'bidResponse') || [];
-  const allNoBids = prebid?.events?.filter(event => event.eventType === 'noBid') || [];
+  const auctionEndEvents = prebid.events.filter(event => event.eventType === 'auctionEnd') || [];
+  const allAvailableBids = prebid.events.filter(event => event.eventType === 'bidResponse') || [];
+  const allNoBids = prebid.events.filter(event => event.eventType === 'noBid') || [];
   const allAdUnits = Array.from(new Set(prebid?.events?.filter(event => event.eventType === 'auctionInit').reduce((previousValue, currentValue) => [].concat(previousValue, currentValue.args.adUnitCodes), [])))
   const allBidders = Array.from(new Set([].concat(allAvailableBids, allNoBids).map(event => event?.args.bidder)))
 
   return (
     <Box>
       {prebid.version && <Typography><strong>Version: </strong>{prebid.version}</Typography>}
-      {prebid?.config?.timeout && <Typography><strong>Timeout: </strong>{prebid.config.timeout}</Typography>}
+      {prebid.config?.timeout && <Typography><strong>Timeout: </strong>{prebid.config.timeout}</Typography>}
       {auctionEndEvents.map((event, index) =>
         <Box key={index}>
           <Typography><strong>Auction Time: </strong>{event.args.auctionEnd - event.args.timestamp} ms</Typography>
-          <Typography><strong>Auction Start Time: </strong>{event.args.timestamp}</Typography>
-          <Typography><strong>Auction End Time: </strong>{event.args.auctionEnd}</Typography>
+          <Typography><strong>Auction Start Time: </strong>{new Date(event.args.timestamp).toISOString()}</Typography>
+          <Typography><strong>Auction End Time: </strong>{new Date(event.args.auctionEnd).toISOString()}</Typography>
           <Typography><strong>AdUnits Detected: </strong> {allAdUnits.length}</Typography>
           <Typography><strong>Bidders: </strong>{allBidders.length}</Typography>
           <Typography><strong>NoBid / Bid Ratio: </strong>{allNoBids.length} / {allAvailableBids.length}</Typography>
         </Box>
       )}
-      <PrebidDetailsSlotsComponent prebid={prebid}></PrebidDetailsSlotsComponent>
-      <PrebidDetailsBidsComponent prebid={prebid}></PrebidDetailsBidsComponent>
+      {prebid.events[0] && <PrebidDetailsSlotsComponent prebid={prebid}></PrebidDetailsSlotsComponent>}
+      {prebid.events[0] && <PrebidDetailsBidsComponent prebid={prebid}></PrebidDetailsBidsComponent>}
     </Box>
   )
 };
