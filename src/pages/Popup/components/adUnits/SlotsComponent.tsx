5752fc8a1801d65424f954b380a5eb9e6223a25c
Florian Erl <46747754+florianerl@users.noreply.github.com>
2021-10-26 17:25:07 +0200

    clean up

--- src/pages/Popup/components/adUnits/SlotsComponent.tsx
+++ src/pages/Popup/components/adUnits/SlotsComponent.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { IPrebidDetails } from "../../../../inject/scripts/prebid";
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -15,44 +13,41 @@ import Stack from '@mui/material/Stack';
 const SlotsComponent = ({ prebid }: ISlotsComponentProps): JSX.Element => {
   const adUnits = prebid.events.filter(event => event.eventType === 'auctionEnd').map(event => event.args.adUnits).flat() || [];
   return (
-    <Box>
-      <Typography><strong>Slots</strong></Typography>
-      <TableContainer>
-        <Table size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell variant="head">Code</TableCell>
-              <TableCell variant="head">Media Types</TableCell>
-              <TableCell variant="head">Bidders</TableCell>
+    <TableContainer>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell variant="head">Code</TableCell>
+            <TableCell variant="head">Media Types</TableCell>
+            <TableCell variant="head">Bidders</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {adUnits.map((adUnit: any, index) => (
+            <TableRow key={index} sx={{ verticalAlign: 'top', '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell variant="body">{adUnit.code}</TableCell>
+              <TableCell variant="body">
+                Banner Sizes:
+                <Stack direction="row" sx={{ flexWrap: 'wrap', gap: '5px' }}>
+                  {adUnit.mediaTypes?.banner?.sizes?.map((size: string[], index: number) =>
+                    <Chip size="small" key={index} label={size[0] + 'x' + size[1]} variant="outlined" sx={{ minWidth: '84px' }} />
+                  )}
+                </Stack>
+              </TableCell>
+              <TableCell variant="body">
+                <Stack direction="row" sx={{ flexWrap: 'wrap', gap: '5px' }}>
+                  {Array.from(new Set(adUnit.bids)).map((bid: any, index: number) =>
+                    <Tooltip title={JSON.stringify(bid.params, null, 4)} key={index}>
+                      <Chip size="small" label={bid.bidder} variant="outlined" />
+                    </Tooltip>
+                  )}
+                </Stack>
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {adUnits.map((adUnit: any, index) => (
-              <TableRow key={index} sx={{ verticalAlign: 'top', '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell variant="body">{adUnit.code}</TableCell>
-                <TableCell variant="body">
-                  Banner Sizes:
-                  <Stack direction="row" sx={{ flexWrap: 'wrap', gap: '5px' }}>
-                    {adUnit.mediaTypes?.banner?.sizes?.map((size: string[], index: number) =>
-                      <Chip size="small" key={index} label={size[0] + 'x' + size[1]} variant="outlined" sx={{ minWidth: '84px' }} />
-                    )}
-                  </Stack>
-                </TableCell>
-                <TableCell variant="body">
-                  <Stack direction="row" sx={{ flexWrap: 'wrap', gap: '5px' }}>
-                    {Array.from(new Set(adUnit.bids)).map((bid: any, index: number) =>
-                      <Tooltip title={JSON.stringify(bid.params, null, 4)} key={index}>
-                        <Chip size="small" label={bid.bidder} variant="outlined" />
-                      </Tooltip>
-                    )}
-                  </Stack>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 };
 
