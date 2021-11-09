c044db0b635665ec2136865b5228f1d9b883c72d
Florian Erl <46747754+florianerl@users.noreply.github.com>
2021-11-09 15:56:03 +0100

    fallback to container element  with id in substring of element id

--- src/inject/app/InjectedApp.tsx
+++ src/inject/app/InjectedApp.tsx
@@ -8,7 +8,7 @@ import Box from '@mui/material/Box';
 
 const InjectedApp = (): JSX.Element => {
   logger.log('[InjectedApp] init');
-
+  let findContainerCount = 0;
   const [consoleState, setConsoleState] = useState(false);
   const handleConsoleStateChange = useCallback(event => {
     const checked = (event as CustomEvent).detail;
@@ -33,13 +33,32 @@ const InjectedApp = (): JSX.Element => {
     return () => {
       document.removeEventListener(constants.SAVE_MASKS, handleNewMasks);
     }
-  },[]);
+  }, []);
+
+  const findContainerElement = (id: string) => {
+    const container = document.getElementById(id);
+    const elementsWithIdSubstring = document.querySelectorAll(`[id*="${id}"]`);
+    if (container) {
+      return container;
+    }
+    if (elementsWithIdSubstring.length > 0) {
+      return elementsWithIdSubstring[0];
+    }
+    if (findContainerCount > 10) {
+      logger.log(`[InjectedApp] Could not find container element with id ${id}. Retry #${findContainerCount}/10`);
+      requestIdleCallback(() => {
+        findContainerElement(id);
+        findContainerCount += 1;
+      });
+    }
+    return null;
+  }
 
   return (
     <Box>
-      {masks.map((mask, index) =>
-        <AdMaskPortal key={index} container={document.getElementById(mask.elementId)} mask={mask} consoleState={consoleState} />
-      )}
+      {masks.map((mask, index) => {
+        return <AdMaskPortal key={index} container={findContainerElement(mask.elementId) as HTMLDivElement} mask={mask} consoleState={consoleState} />
+      })}
     </Box>
   );
 };
