fb3a7139c0e01e9669db4e3d9ce3340a0bf2248b
Florian Erl <46747754+florianerl@users.noreply.github.com>
2021-12-02 10:51:57 +0100

    arrow functions

--- src/pages/Popup/components/bids/BidsComponent.tsx
+++ src/pages/Popup/components/bids/BidsComponent.tsx
@@ -26,7 +26,7 @@ interface TabPanelProps {
   value: number;
 }
 
-function TabPanel(props: TabPanelProps) {
+const TabPanel =(props: TabPanelProps) => {
   const { children, value, index, ...other } = props;
 
   return (
@@ -40,7 +40,7 @@ function TabPanel(props: TabPanelProps) {
   );
 }
 
-function a11yProps(index: number) {
+const a11yProps = (index: number) => {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
