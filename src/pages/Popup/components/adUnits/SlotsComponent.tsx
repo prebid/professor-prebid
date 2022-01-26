2a0e565d2c2d94657586f4d4b2b5e8eef111575a
Florian Erl <46747754+florianerl@users.noreply.github.com>
2022-01-26 13:47:34 +0100

    add click to scroll to element

--- src/pages/Popup/components/adUnits/SlotsComponent.tsx
+++ src/pages/Popup/components/adUnits/SlotsComponent.tsx
@@ -20,8 +20,57 @@ import ReactJson from 'react-json-view';
 import Popover from '@mui/material/Popover';
 import GavelOutlinedIcon from '@mui/icons-material/GavelOutlined';
 
-const ChipWithPopOverOnClickComponent = ({ input, label, showInputInChip, isWinner }: any): JSX.Element => {
-  const labelText = showInputInChip ? `${label}: ${JSON.stringify(input)}` : `${label}`;
+const AdUnitChipComponent = ({ adUnit }: any): JSX.Element => {
+  const [labelText, setLabelText] = React.useState<string | null>(adUnit.code);
+  const scroll2element = (elementId: string): any => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentTab = tabs[0];
+      const tabId = currentTab.id;
+      const func = (elementId: string) => {
+        const element = document.getElementById(elementId);
+        const addBorder = (element: HTMLElement) => {
+          const borderStyleBak = element.style.border;
+          element?.scrollIntoView();
+          element.style.border = '5px dashed #f99b0c';
+          element.animate([{ transform: 'rotate(0deg)' }, { transform: 'rotate(360deg)' }], {
+            duration: 1000,
+            iterations: 1, // Infinity
+          });
+          window.setTimeout(() => {
+            element.style.border = borderStyleBak;
+          }, 5000);
+        };
+        if (!element) {
+          return false;
+        } else {
+          addBorder(element);
+          return true;
+        }
+      };
+      chrome.scripting.executeScript({ target: { tabId }, func, args: [elementId] }, (injectionResults) => {
+        if (injectionResults[0].result) {
+          setLabelText(`✓ ${adUnit.code}`);
+        } else {
+          setLabelText(`✗ Element with id="${adUnit.code}" not found.`);
+        }
+        setTimeout(() => setLabelText(adUnit.code), 1500);
+      });
+    });
+  };
+  return (
+    <Chip
+      onClick={() => {
+        scroll2element(adUnit.code);
+      }}
+      label={labelText}
+      variant="outlined"
+      sx={{ transition: 'all linear 0.25s' }}
+    />
+  );
+};
+
+const BidChipComponent = ({ input, label, isWinner, bidReceived }: any): JSX.Element => {
+  const labelText = `${label}`;
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
   const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
     if (typeof input === 'object') {
@@ -38,12 +87,55 @@ const ChipWithPopOverOnClickComponent = ({ input, label, showInputInChip, isWinn
       <Chip
         size="small"
         variant="outlined"
-        color={isWinner ? 'secondary' : 'primary'}
+        color={isWinner ? 'secondary' : bidReceived ? 'primary' : 'default'}
         icon={isWinner ? <GavelOutlinedIcon sx={{ height: '12px', paddingLeft: 1 }} /> : null}
         label={labelText}
         onClick={handlePopoverOpen}
         sx={{ maxWidth: 200 }}
       />
+      <Popover
+        id="mouse-over-popover"
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <ReactJson
+          src={input}
+          name={false}
+          collapsed={false}
+          enableClipboard={false}
+          displayObjectSize={false}
+          displayDataTypes={false}
+          sortKeys={false}
+          quotesOnKeys={false}
+          indentWidth={2}
+          collapseStringsAfterLength={100}
+          style={{ fontSize: '12px', fontFamily: 'roboto', padding: '5px' }}
+        />
+      </Popover>
+    </React.Fragment>
+  );
+};
+
+const MediaTypeChipComponent = ({ input, label }: any): JSX.Element => {
+  const labelText = `${label}: ${JSON.stringify(input)}`;
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
+    if (typeof input === 'object') {
+      setAnchorEl(event.currentTarget);
+    }
+  };
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  return (
+    <React.Fragment>
+      <Chip size="small" variant="outlined" color="primary" label={labelText} onClick={handlePopoverOpen} sx={{ maxWidth: 200 }} />
       <Popover
         id="mouse-over-popover"
         open={open}
@@ -94,12 +186,7 @@ const MediaTypesComponent = ({ mediaTypes }: IMediaTypesComponentProps): JSX.Ele
                 <Typography variant="subtitle2">Video:</Typography>
                 <Stack direction="row" sx={{ flexWrap: 'wrap', gap: '5px' }}>
                   {Object.keys(mediaTypes[mediaType]).map((key, index) => (
-                    <ChipWithPopOverOnClickComponent
-                      input={mediaTypes[mediaType][key as keyof IPrebidAdUnitMediaTypes['video']]}
-                      showInputInChip={true}
-                      label={key}
-                      key={index}
-                    />
+                    <MediaTypeChipComponent input={mediaTypes[mediaType][key as keyof IPrebidAdUnitMediaTypes['video']]} label={key} key={index} />
                   ))}
                 </Stack>
               </Box>
@@ -111,12 +198,7 @@ const MediaTypesComponent = ({ mediaTypes }: IMediaTypesComponentProps): JSX.Ele
                 <Typography variant="subtitle2">Native:</Typography>
                 <Stack direction="row" sx={{ flexWrap: 'wrap', gap: '5px' }}>
                   {Object.keys(mediaTypes[mediaType]).map((key, index) => (
-                    <ChipWithPopOverOnClickComponent
-                      input={mediaTypes[mediaType][key as keyof IPrebidAdUnitMediaTypes['native']]}
-                      showInputInChip={true}
-                      label={key}
-                      key={index}
-                    />
+                    <MediaTypeChipComponent input={mediaTypes[mediaType][key as keyof IPrebidAdUnitMediaTypes['native']]} label={key} key={index} />
                   ))}
                 </Stack>
               </Box>
@@ -151,7 +233,7 @@ const SlotsComponent = ({ adUnits, latestAuctionsWinningBids, latestAuctionsBids
             return (
               <TableRow key={index} sx={{ verticalAlign: 'top', '&:last-child td, &:last-child th': { border: 0 } }}>
                 <TableCell variant="body">
-                  <strong>{adUnit.code}</strong>
+                  <AdUnitChipComponent adUnit={adUnit} />
                 </TableCell>
                 <TableCell variant="body">
                   <MediaTypesComponent mediaTypes={adUnit.mediaTypes} />
@@ -174,7 +256,7 @@ const SlotsComponent = ({ adUnits, latestAuctionsWinningBids, latestAuctionsBids
                       const label = bidReceived?.args.cpm
                         ? `${bid.bidder} (${bidReceived?.args.cpm.toFixed(2)} ${bidReceived?.args.currency})`
                         : `${bid.bidder}`;
-                      return <ChipWithPopOverOnClickComponent input={bid} label={label} key={index} isWinner={isWinner} />;
+                      return <BidChipComponent input={bid} label={label} key={index} isWinner={isWinner} bidReceived={bidReceived} />;
                     })}
                   </Stack>
                 </TableCell>
