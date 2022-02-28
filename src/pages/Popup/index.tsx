f2b5e0edbe782dee2c83893a4c410e7d5d50688b
Florian Erl <46747754+florianerl@users.noreply.github.com>
2022-02-28 18:52:23 +0100

    set body width to window width

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Popup } from './Popup';
-import './index.scss';
 ReactDOM.render(<Popup />, document.getElementById('root'));
 const handleResize = () => {
   const width = window.innerWidth;
   const body = document.querySelector('body');
-  if (width < 900 && width > 424) {
-    body.style.width = '800px';
-    body.style.height = '600px';
-    body.style.margin = '0px';
-  } else if (width < 425) {
-    body.style.width = '378px';
-    body.style.height = '600px';
-    body.style.margin = '0px';
-  }
+  body.style.width = width + 'px';
 };
-handleResize();
+const interval = setInterval(() => handleResize, 1000);
 window.addEventListener('resize', handleResize);
+window.addEventListener('resize', () => clearInterval(interval));
