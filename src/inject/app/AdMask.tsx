623470698e5b881a4deac1f48aba00552a602d75
Tim Asp <707699+timothyasp@users.noreply.github.com>
2022-05-18 18:10:01 -0700

    [Style] Compress overlay labels
    Benefits smaller ad unit overlays

--- src/inject/app/AdMask.tsx
+++ src/inject/app/AdMask.tsx
@@ -6,25 +6,24 @@ const AdMaskComponent = ({ creativeRenderTime, elementId, winningCPM, winningBid
   return (
     <Box sx={{ textAlign: 'left', opacity: 1 }}>
       <Typography>
-        <strong>Element Id: </strong>
         {elementId}
       </Typography>
       {/* <Typography><strong>creativeRenderTime: </strong>{creativeRenderTime}</Typography> */}
       {winningCPM && (
         <Typography>
-          <strong>Winning CPM: </strong>
+          <strong>CPM </strong>
           {winningCPM} {currency}
         </Typography>
       )}
       {winningBidder && (
         <Typography>
-          <strong>Winning Bidder: </strong>
+          <strong>Bidder </strong>
           {winningBidder}
         </Typography>
       )}
       {timeToRespond && (
         <Typography>
-          <strong>Time To Respond: </strong>
+          <strong>TTR </strong>
           {timeToRespond}ms
         </Typography>
       )}
