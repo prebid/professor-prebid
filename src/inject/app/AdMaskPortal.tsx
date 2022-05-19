2f4888253bba85f3c59d67a0520d9d955e2fbdc7
Tim Asp <707699+timothyasp@users.noreply.github.com>
2022-05-18 18:41:57 -0700

    [Style] Tweak injected overlay style
    Give the text some more contrast and tighten up the sizing so it covers less of the ad.

--- src/inject/app/AdMaskPortal.tsx
+++ src/inject/app/AdMaskPortal.tsx
@@ -17,14 +17,10 @@ const AdMaskPortal: React.FC<IAdMaskPortalProps> = ({ container, mask, consoleSt
         element.current.style.width = `${width}px`;
         element.current.style.height = `${height}px`;
         element.current.style.display = height < 100 ? 'inline-block' : 'block';
-        element.current.style.opacity = '0.7';
         element.current.style.position = 'absolute';
-        element.current.style.zIndex = '100000000';
-        element.current.style.padding = '4px 8px';
+        element.current.style.zIndex = '2147483647';
         element.current.style.wordBreak = 'break-all';
         element.current.style.overflow = 'hidden';
-        element.current.style.backgroundColor = '#f99b0c';
-        element.current.style.color = '#797f90;';
         element.current.classList.add('prpb-mask__overlay');
         element.current.id = `prpb-mask--container-${mask.elementId}`;
         container?.prepend(element.current);
