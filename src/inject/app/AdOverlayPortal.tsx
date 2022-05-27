9466d574a4dc23db574a62b376194c2e0ada6377
f.erl <f.erl@criteo.com>
2022-05-27 17:45:10 +0200

    encapsulate overlay in iframe

--- src/inject/app/AdOverlayPortal.tsx
+++ src/inject/app/AdOverlayPortal.tsx
@@ -2,8 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import AdOverlayComponent, { AdOverlayComponentProps } from './AdOverlayComponent';
 import logger from '../../logger';
 import { createPortal } from 'react-dom';
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
+
 export const getMaxZIndex = () =>
   Math.max(
     ...Array.from(document.querySelectorAll('*'), (el) => parseFloat(window.getComputedStyle(el).zIndex)).filter((zIndex) => !Number.isNaN(zIndex)),
@@ -12,12 +11,11 @@ export const getMaxZIndex = () =>
 
 const AdOverlayPortal: React.FC<AdOverlayComponentPropsProps> = ({ container, mask, consoleState }) => {
   const [contentRef, setContentRef] = useState(null);
-  const mountNode = contentRef?.contentWindow?.document?.body;
-  if (mountNode) mountNode.style.margin = '0';
+  const iFrameBody = contentRef?.contentWindow?.document?.body;
+  if (iFrameBody) iFrameBody.style.margin = '0';
 
   const { elementId, winningCPM, winningBidder, currency, timeToRespond } = mask;
   const element = useRef<HTMLDivElement>(document.createElement('div'));
-  const cache = createCache({ key: 'css', container: contentRef?.contentWindow?.document?.head, prepend: true });
   const closePortal = () => {
     document.getElementById(`prpb-mask--container-${mask.elementId}`).style.display = 'none';
   };
@@ -41,7 +39,6 @@ const AdOverlayPortal: React.FC<AdOverlayComponentPropsProps> = ({ container, ma
     }
   }, [mask, consoleState, container]);
 
-
   return createPortal(
     <iframe
       title={element.current.id}
@@ -51,21 +48,19 @@ const AdOverlayPortal: React.FC<AdOverlayComponentPropsProps> = ({ container, ma
       scrolling="no"
       frameBorder="0"
     >
-      {mountNode &&
+      {iFrameBody &&
         createPortal(
-          <CacheProvider value={cache}>
-            <AdOverlayComponent
-              key={`AdMask-${elementId}`}
-              elementId={elementId}
-              winningCPM={winningCPM}
-              winningBidder={winningBidder}
-              currency={currency}
-              timeToRespond={timeToRespond}
-              closePortal={closePortal}
-              contentRef={contentRef}
-            />
-          </CacheProvider>,
-          mountNode
+          <AdOverlayComponent
+            key={`AdMask-${elementId}`}
+            elementId={elementId}
+            winningCPM={winningCPM}
+            winningBidder={winningBidder}
+            currency={currency}
+            timeToRespond={timeToRespond}
+            closePortal={closePortal}
+            contentRef={contentRef}
+          />,
+          iFrameBody
         )}
     </iframe>,
     element.current
