3423496b13e4434d9c3eb408aa13b3ef6cb61024
f.erl <f.erl@criteo.com>
2022-05-27 17:38:30 +0200

    encapsulate overlay in iframe

--- src/inject/app/AdOverlayComponent.tsx
+++ src/inject/app/AdOverlayComponent.tsx
@@ -8,92 +8,108 @@ import HeaderRowComponent from './HeaderRowComponent';
 import { Paper } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import PopOverComponent from './PopOverComponent';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 
-const AdOverlayComponent = ({ elementId, winningCPM, winningBidder, currency, timeToRespond, closePortal }: AdOverlayComponentProps): JSX.Element => {
+const AdOverlayComponent = ({
+  elementId,
+  winningCPM,
+  winningBidder,
+  currency,
+  timeToRespond,
+  closePortal,
+  contentRef,
+}: AdOverlayComponentProps): JSX.Element => {
   const [expanded, setExpanded] = useState<boolean>(true);
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
   const openInPopOver = () => {
-    setAnchorEl(document.body);
+    setAnchorEl(window.top.document.body);
   };
   const closePopOver = () => {
     setAnchorEl(null);
   };
+  const cache = createCache({ key: 'css', container: contentRef?.contentWindow?.document?.head, prepend: true });
+  const cacheTopPage = createCache({ key: 'css', container: window.top.document?.head, prepend: true });
 
   return (
     <ThemeProvider theme={overlayTheme}>
-      <PopOverComponent
-        elementId={elementId}
-        winningCPM={winningCPM}
-        winningBidder={winningBidder}
-        currency={currency}
-        timeToRespond={timeToRespond}
-        closePortal={closePortal}
-        anchorEl={anchorEl}
-        setAnchorEl={setAnchorEl}
-      />
-      <Box
-        sx={{
-          height: expanded ? '100%' : 'auto',
-          width: '100%',
-          opacity: 0.9,
-          backgroundColor: 'primary.light',
-          color: 'text.primary',
-          padding: 0.5,
-          boxSizing: 'border-box',
-          flexGrow: 1,
-          '&:hover': { opacity: 1 },
-          display: 'flex',
-          flexDirection: 'column',
-          overflow: 'hidden',
-        }}
-      >
-        <Grid container columnSpacing={0.5} justifyContent="flex-start" alignItems="flex-start">
-          <HeaderRowComponent
-            elementId={elementId}
-            expanded={expanded}
-            setExpanded={setExpanded}
-            openInPopOver={openInPopOver}
-            closePortal={closePortal}
-            closePopOver={closePopOver}
-            inPopOver={false}
-          />
-          {expanded && (currency || winningBidder || winningCPM || timeToRespond || elementId) && (
-            <Grid container item xs={12} spacing={0.5}>
-              {winningCPM && (
-                <Grid item>
-                  <Paper elevation={1} sx={{ p: 0.5 }}>
-                    <Typography>
-                      <strong>CPM: </strong>
-                      {winningCPM} {currency}
-                    </Typography>
-                  </Paper>
-                </Grid>
-              )}
-              {winningBidder && (
-                <Grid item>
-                  <Paper elevation={1} sx={{ p: 0.5 }}>
-                    <Typography>
-                      <strong>Bidder: </strong>
-                      {winningBidder}
-                    </Typography>
-                  </Paper>
-                </Grid>
-              )}
-              {timeToRespond && (
-                <Grid item>
-                  <Paper elevation={1} sx={{ p: 0.5 }}>
-                    <Typography>
-                      <strong>TTR: </strong>
-                      {timeToRespond}ms
-                    </Typography>
-                  </Paper>
-                </Grid>
-              )}
-              {elementId && <GamDetailsComponent elementId={elementId} inPopOver={false} />}
-            </Grid>
-          )}
-        </Grid>
-      </Box>
+      <CacheProvider value={cacheTopPage}>
+        <PopOverComponent
+          elementId={elementId}
+          winningCPM={winningCPM}
+          winningBidder={winningBidder}
+          currency={currency}
+          timeToRespond={timeToRespond}
+          closePortal={closePortal}
+          anchorEl={anchorEl}
+          setAnchorEl={setAnchorEl}
+        />
+      </CacheProvider>
+      <CacheProvider value={cache}>
+        <Box
+          sx={{
+            height: expanded ? '100%' : 'auto',
+            width: '100%',
+            opacity: 0.9,
+            backgroundColor: 'primary.light',
+            color: 'text.primary',
+            padding: 0.5,
+            boxSizing: 'border-box',
+            flexGrow: 1,
+            '&:hover': { opacity: 1 },
+            display: 'flex',
+            flexDirection: 'column',
+            overflow: 'hidden',
+          }}
+        >
+          <Grid container columnSpacing={0.5} justifyContent="flex-start" alignItems="flex-start">
+            <HeaderRowComponent
+              elementId={elementId}
+              expanded={expanded}
+              setExpanded={setExpanded}
+              openInPopOver={openInPopOver}
+              closePortal={closePortal}
+              closePopOver={closePopOver}
+              inPopOver={false}
+            />
+            {expanded && (currency || winningBidder || winningCPM || timeToRespond || elementId) && (
+              <Grid container item xs={12} spacing={0.5}>
+                {winningCPM && (
+                  <Grid item>
+                    <Paper elevation={1} sx={{ p: 0.5 }}>
+                      <Typography>
+                        <strong>CPM: </strong>
+                        {winningCPM} {currency}
+                      </Typography>
+                    </Paper>
+                  </Grid>
+                )}
+                {winningBidder && (
+                  <Grid item>
+                    <Paper elevation={1} sx={{ p: 0.5 }}>
+                      <Typography>
+                        <strong>Bidder: </strong>
+                        {winningBidder}
+                      </Typography>
+                    </Paper>
+                  </Grid>
+                )}
+                {timeToRespond && (
+                  <Grid item>
+                    <Paper elevation={1} sx={{ p: 0.5 }}>
+                      <Typography>
+                        <strong>TTR: </strong>
+                        {timeToRespond}ms
+                      </Typography>
+                    </Paper>
+                  </Grid>
+                )}
+                {elementId && <GamDetailsComponent elementId={elementId} inPopOver={false} />}
+              </Grid>
+            )}
+          </Grid>
+        </Box>
+      </CacheProvider>
     </ThemeProvider>
   );
 };
@@ -105,6 +121,7 @@ export interface AdOverlayComponentProps {
   currency: string;
   timeToRespond: number;
   closePortal?: () => void;
+  contentRef?: any;
 }
 
 export default AdOverlayComponent;
--- src/inject/app/AdOverlayPortal.tsx
+++ src/inject/app/AdOverlayPortal.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useRef, useState } from 'react';
 import AdOverlayComponent, { AdOverlayComponentProps } from './AdOverlayComponent';
 import logger from '../../logger';
-
+import { createPortal } from 'react-dom';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 export const getMaxZIndex = () =>
   Math.max(
     ...Array.from(document.querySelectorAll('*'), (el) => parseFloat(window.getComputedStyle(el).zIndex)).filter((zIndex) => !Number.isNaN(zIndex)),
@@ -10,11 +11,17 @@ export const getMaxZIndex = () =>
   );
 
 const AdOverlayPortal: React.FC<AdOverlayComponentPropsProps> = ({ container, mask, consoleState }) => {
+  const [contentRef, setContentRef] = useState(null);
+  const mountNode = contentRef?.contentWindow?.document?.body;
+  if (mountNode) mountNode.style.margin = '0';
+
   const { elementId, winningCPM, winningBidder, currency, timeToRespond } = mask;
   const element = useRef<HTMLDivElement>(document.createElement('div'));
+  const cache = createCache({ key: 'css', container: contentRef?.contentWindow?.document?.head, prepend: true });
   const closePortal = () => {
     document.getElementById(`prpb-mask--container-${mask.elementId}`).style.display = 'none';
   };
+
   useEffect(() => {
     logger.log('[AdMaskPortal] Mounting AdMaskPortal');
     const slotMaskElement = document.getElementById(`prpb-mask--container-${mask.elementId}`);
@@ -34,16 +41,33 @@ const AdOverlayPortal: React.FC<AdOverlayComponentPropsProps> = ({ container, ma
     }
   }, [mask, consoleState, container]);
 
-  return ReactDOM.createPortal(
-    <AdOverlayComponent
-      key={`AdMask-${elementId}`}
-      elementId={elementId}
-      winningCPM={winningCPM}
-      winningBidder={winningBidder}
-      currency={currency}
-      timeToRespond={timeToRespond}
-      closePortal={closePortal}
-    />,
+
+  return createPortal(
+    <iframe
+      title={element.current.id}
+      ref={setContentRef}
+      width={`${container?.offsetWidth || container?.clientWidth}px`}
+      height={`${container?.offsetHeight || container?.clientHeight}px`}
+      scrolling="no"
+      frameBorder="0"
+    >
+      {mountNode &&
+        createPortal(
+          <CacheProvider value={cache}>
+            <AdOverlayComponent
+              key={`AdMask-${elementId}`}
+              elementId={elementId}
+              winningCPM={winningCPM}
+              winningBidder={winningBidder}
+              currency={currency}
+              timeToRespond={timeToRespond}
+              closePortal={closePortal}
+              contentRef={contentRef}
+            />
+          </CacheProvider>,
+          mountNode
+        )}
+    </iframe>,
     element.current
   );
 };
