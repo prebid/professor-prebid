304b7d0522ae97e44098f303ed7d3bddb92ea7d7
f.erl <f.erl@criteo.com>
2023-06-12 16:29:57 +0200

    wrap each tile in error boundary

--- src/pages/Shared/components/config/ConfigComponent.tsx
+++ src/pages/Shared/components/config/ConfigComponent.tsx
@@ -8,26 +8,54 @@ import PrebidConfigComponent from '../../../Popup/components/config/PrebidConfig
 import FloorsModuleComponent from './tiles/FloorsModuleComponent';
 import GptPreAuctionComponent from './tiles/GptPreAuctionComponent';
 import Grid from '@mui/material/Grid';
+import { ErrorBoundary } from 'react-error-boundary';
 
 export const tileHeight = 255;
 
 const ConfigComponent = (): JSX.Element => (
   <Grid container spacing={0.25} padding={0.5}>
-    <PriceGranularityComponent />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <PriceGranularityComponent />
+    </ErrorBoundary>
 
-    <PrebidConfigComponent />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <PrebidConfigComponent />
+    </ErrorBoundary>
 
-    <BidderSettingsComponent />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <BidderSettingsComponent />
+    </ErrorBoundary>
 
-    <PrebidServerComponent />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <PrebidServerComponent />
+    </ErrorBoundary>
 
-    <PrivacyComponent />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <PrivacyComponent />
+    </ErrorBoundary>
 
-    <UserIdModule />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <UserIdModule />
+    </ErrorBoundary>
 
-    <FloorsModuleComponent />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <FloorsModuleComponent />
+    </ErrorBoundary>
 
-    <GptPreAuctionComponent />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <GptPreAuctionComponent />
+    </ErrorBoundary>
   </Grid>
 );
 export default ConfigComponent;
+
+const Fallback = ({ error }: { error: any }) => {
+  return (
+    <Grid item sm={4} xs={12}>
+      <div role="alert">
+        <p>Something went wrong:</p>
+        <pre style={{ color: 'red' }}>{error.message}</pre>
+      </div>
+    </Grid>
+  );
+};
