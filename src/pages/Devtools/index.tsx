d0ae5ec7c9d6d327d79dd6e74226aea79a621d26
f.erl <f.erl@criteo.com>
2023-06-12 16:29:33 +0200

    clean up

--- src/pages/Devtools/index.tsx
+++ src/pages/Devtools/index.tsx
@@ -1,18 +1,1 @@
-let windowObj: Window;
-let tabId: number;
-export const createPanel = () =>
-  chrome.devtools?.panels.create('Professor Prebid', 'icon-34.png', 'panel.html', async (panel) => {
-    tabId = chrome.devtools.inspectedWindow.tabId;
-    panel.onShown.addListener(function tmp(panel_window) {
-      panel.onShown.removeListener(tmp);
-      windowObj = panel_window;
-      windowObj.tabId = tabId;
-    });
-  });
-createPanel();
-
-chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
-  if (details.tabId === tabId && details.frameId === 0) {
-    windowObj?.location?.reload();
-  }
-});
+chrome.devtools?.panels?.create('Professor Prebid', 'icon-34.png', 'panel.html', async (panel) => {});
--- src/pages/Shared/components/ErrorCardComponent.tsx
+++ src/pages/Shared/components/ErrorCardComponent.tsx
@@ -1,47 +1,19 @@
-import React, { useContext, useRef } from 'react';
-import { styled } from '@mui/material/styles';
+import React, { useContext } from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import Box from '@mui/material/Box';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import BugReportIcon from '@mui/icons-material/BugReport';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import InspectedPageContext from '../contexts/inspectedPageContext';
-import JSONViewerComponent from './JSONViewerComponent';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import LogoSvg from '../../../assets/Logo';
 import { reloadPage } from '../utils';
 import { Avatar } from '@mui/material';
 
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
 const ErrorCardComponent = ({ error }: any) => {
   const context = useContext(InspectedPageContext);
   const { syncState } = context;
-  const [expanded, setExpanded] = React.useState(false);
-  const cardRef = useRef(null);
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  console.log({ cardRef });
 
   return (
     <Box sx={{ backgroundColor: 'primary.light' }}>
@@ -59,7 +31,7 @@ const ErrorCardComponent = ({ error }: any) => {
         }}
       ></Box>
       <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#ABDDF' }}>
-        <Card sx={{ maxWidth: 0.75, minWidth: 0.75 }} ref={cardRef}>
+        <Card sx={{ maxWidth: 0.75, minWidth: 0.75 }}>
           <CardHeader
             avatar={
               <Avatar sx={{ backgroundColor: 'primary.light', width: 70, height: 70 }}>
@@ -82,11 +54,12 @@ const ErrorCardComponent = ({ error }: any) => {
           />
           <CardContent>
             <Typography variant="body1" paragraph>
-              We apologize for the inconvenience, but it seems that an error has occurred.We highly value your privacy and therefore do not track any
+              We apologize for the inconvenience, but it seems that an error has occurred. We highly value your privacy and therefore do not track any
               bugs or user activities.
             </Typography>
             <Typography variant="body1" paragraph>
-              To help us improve your experience and resolve this issue, we kindly request you to consider opening an issue on our GitHub page.
+              To help us improve your experience and resolve this issue, we kindly request you to consider opening an issue on our{' '}
+              <span onClick={() => chrome.tabs.create({ url: 'https://github.com/prebid/professor-prebid/issues' })}>GitHub page</span>.
             </Typography>
             <Typography variant="body1" paragraph>
               Your feedback is crucial in enhancing our extension and ensuring a seamless browsing experience for all our users.
@@ -94,29 +67,7 @@ const ErrorCardComponent = ({ error }: any) => {
             <Typography variant="body1" paragraph>
               Thank you for your understanding and support!
             </Typography>
-            <Typography variant="body1" paragraph>
-              If you would like to help us resolve this issue, please click the button below to expand the error details and copy the contents of the
-              error message.
-            </Typography>
           </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="report bug">
-              <BugReportIcon />
-            </IconButton>
-            <ExpandMore expand={expanded} onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
-              <ExpandMoreIcon />
-            </ExpandMore>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Typography paragraph>Error:</Typography>
-              <JSONViewerComponent src={{ error }} collapsed={3} />
-            </CardContent>
-            <CardContent>
-              <Typography paragraph>Details:</Typography>
-              <JSONViewerComponent src={context || { context: JSON.stringify(context) }} collapsed={3} />
-            </CardContent>
-          </Collapse>
         </Card>
       </Box>
     </Box>
