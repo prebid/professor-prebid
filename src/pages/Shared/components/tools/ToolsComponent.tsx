47b263a74e379326de12726c84f0eeecb781777c
f.erl <f.erl@criteo.com>
2023-06-30 16:11:20 +0200

    fix label + unused import

--- src/pages/Shared/components/tools/ToolsComponent.tsx
+++ src/pages/Shared/components/tools/ToolsComponent.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
