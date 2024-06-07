3c642db87234be53f5ba53fe4a5ed953f0890e73
f.erl <f.erl@criteo.com>
2024-06-07 14:22:40 +0200

    remove debug

--- src/pages/App/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DebugComponent from './components/DebugComponent';
-
-ReactDOM.render(<DebugComponent />, document.getElementById('root'));
